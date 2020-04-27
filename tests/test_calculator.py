f0906c4ff26f820eb79884d73008b55889dd80a0
Antoine Mertz <mertz.antoine@gmail.com>
2020-04-27 17:31:01 +0200

    setup|start CI/CD

---
+++ tests/test_calculator.py
@@ -0,0 +1,12 @@
+import sys
+sys.path.append('../src')
+
+import calculator
+
+class TestCalculator:
+
+    def test_add(self):
+        assert 4 == calculator.add(2, 2)
+
+    def test_sub(self):
+        assert 2 == calculator.sub(4, 2)
